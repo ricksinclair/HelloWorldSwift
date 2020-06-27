67b3b620bcdbf6d8061539fa295bdd2150906fe3
Ulric <rick.sinclair.nyc@gmail.com>
2020-06-27 18:30:43 -0400

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Ulric Todman on 6/27/20.
+//  Copyright © 2020 Ulric Todman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
